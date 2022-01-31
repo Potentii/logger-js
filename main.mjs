cf131f1457b06c4b348174168c76c067c9c69b5b
Guilherme Reginaldo Ruella <potentii@gmail.com>
2022-01-31 16:49:40 -0300

    Fixed export

--- main.mjs
+++ main.mjs
@@ -2,7 +2,7 @@ import LoggerJs from './libs/logger-js.mjs';
 import * as formats from './libs/formats.mjs';
 import winston from 'winston';
 
-export default {
+export {
     LoggerJs,
     formats,
     winston,
