d52efa44206ef511dfefe6179bd6c7447b192b5b
Guilherme Reginaldo Ruella <potentii@gmail.com>
2023-12-14 19:30:24 -0300

    Serializing error message on 'human' format

--- libs/log-formats.mjs
+++ libs/log-formats.mjs
@@ -4,11 +4,18 @@ import E_COLORS from "./e-colors.mjs";
 
 
 const humanV1 = format.printf(({ level, message, timestamp, err, code, meta }) => {
-    let err_str = err ? '' + (err.stack || err) : '';
-    if(err?.response?.data){
-        const data = err.response.data;
-        err_str += `\nresponse: ${data && (typeof data == 'object' || Array.isArray(data)) ? JSON.stringify(data) : data}`;
+    let err_str = '';
+    if(err){
+        err_str += (err.stack || err);
+        try{
+            err_str += '\n' + JSON.stringify(err, null, 4);
+        } catch (err2){}
+        // if(err?.response?.data){
+        //     const data = err.response.data;
+        //     err_str += `\nresponse: ${data && (typeof data == 'object' || Array.isArray(data)) ? JSON.stringify(data) : data}`;
+        // }
     }
+
     const payload_str = meta ? JSON.stringify(meta) : '';
     return `${ timestamp.replace(/^(\d{4})-(\d{2})-(\d{2}).(\d{2}):(\d{2}):(\d{2})\.(\d+).+/i, '$1-$2-$3 $4:$5:$6.$7') } [${ E_LEVEL_COLORS[level] || E_COLORS.RESET }${ level?.toUpperCase() }${ E_COLORS.RESET }] [${ code }]: ${ message }${meta?'\n' + payload_str:''}${err?'\n' + err_str:''}`;
 });
