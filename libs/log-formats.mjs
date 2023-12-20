bf68f5fc7a24a874efd257819fc34217fcba1500
Guilherme Reginaldo Ruella <potentii@gmail.com>
2023-12-20 07:29:33 -0300

    Fix: Data was not being logged

--- libs/log-formats.mjs
+++ libs/log-formats.mjs
@@ -3,21 +3,17 @@ import E_LEVEL_COLORS from "./e-level-colors.mjs";
 import E_COLORS from "./e-colors.mjs";
 
 
-const humanV1 = format.printf(({ level, message, timestamp, err, code, meta }) => {
+const humanV1 = format.printf(({ level, message, timestamp, err, code, data }) => {
     let err_str = '';
     if(err){
         err_str += (err.stack || err);
         try{
             err_str += '\n' + JSON.stringify(err, null, 4);
         } catch (err2){}
-        // if(err?.response?.data){
-        //     const data = err.response.data;
-        //     err_str += `\nresponse: ${data && (typeof data == 'object' || Array.isArray(data)) ? JSON.stringify(data) : data}`;
-        // }
     }
 
-    const payload_str = meta ? JSON.stringify(meta) : '';
-    return `${ timestamp.replace(/^(\d{4})-(\d{2})-(\d{2}).(\d{2}):(\d{2}):(\d{2})\.(\d+).+/i, '$1-$2-$3 $4:$5:$6.$7') } [${ E_LEVEL_COLORS[level] || E_COLORS.RESET }${ level?.toUpperCase() }${ E_COLORS.RESET }] [${ code }]: ${ message }${meta?'\n' + payload_str:''}${err?'\n' + err_str:''}`;
+    const data_str = data ? JSON.stringify(data) : '';
+    return `${ timestamp.replace(/^(\d{4})-(\d{2})-(\d{2}).(\d{2}):(\d{2}):(\d{2})\.(\d+).+/i, '$1-$2-$3 $4:$5:$6.$7') } [${ E_LEVEL_COLORS[level] || E_COLORS.RESET }${ level?.toUpperCase() }${ E_COLORS.RESET }] [${ code }]: ${ message }${data?'\n' + data_str:''}${err?'\n' + err_str:''}`;
 });
 
 
