c1b2d2d40c6f2f7bcffa86dd77bf1e55e489a61b
Guilherme Reginaldo Ruella <potentii@gmail.com>
2023-12-20 19:49:29 -0300

    The data for human format is now being logged in the same line as the log text

--- libs/log-formats.mjs
+++ libs/log-formats.mjs
@@ -4,16 +4,19 @@ import E_COLORS from "./e-colors.mjs";
 
 
 const humanV1 = format.printf(({ level, message, timestamp, err, code, data }) => {
-    let err_str = '';
+    const timestampStr = timestamp.replace(/^(\d{4})-(\d{2})-(\d{2}).(\d{2}):(\d{2}):(\d{2})\.(\d+).+/i, '$1-$2-$3 $4:$5:$6.$7');
+    const levelColor = E_LEVEL_COLORS[level] || E_COLORS.RESET;
+    const levelStr = level?.toUpperCase();
+    const dataStr = data ? `(${JSON.stringify(data)})` : '';
+    let errStr = '';
     if(err){
-        err_str += (err.stack || err);
+        errStr += '\n' + (err.stack || err);
         try{
-            err_str += '\n' + JSON.stringify(err, null, 4);
+            errStr += '\n' + JSON.stringify(err, null, 4);
         } catch (err2){}
     }
 
-    const data_str = data ? JSON.stringify(data) : '';
-    return `${ timestamp.replace(/^(\d{4})-(\d{2})-(\d{2}).(\d{2}):(\d{2}):(\d{2})\.(\d+).+/i, '$1-$2-$3 $4:$5:$6.$7') } [${ E_LEVEL_COLORS[level] || E_COLORS.RESET }${ level?.toUpperCase() }${ E_COLORS.RESET }] [${ code }]: ${ message }${data?'\n' + data_str:''}${err?'\n' + err_str:''}`;
+    return `${timestampStr} [${levelColor}${levelStr}${E_COLORS.RESET}] [${code}]: ${message}${dataStr}${errStr}`;
 });
 
 
