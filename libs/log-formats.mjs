97cfd6b96c4526c2139e52bdc5a17de32224bd6e
Guilherme Reginaldo Ruella <potentii@gmail.com>
2023-12-20 19:55:30 -0300

    Improved the human format

--- libs/log-formats.mjs
+++ libs/log-formats.mjs
@@ -7,7 +7,7 @@ const humanV1 = format.printf(({ level, message, timestamp, err, code, data }) =
     const timestampStr = timestamp.replace(/^(\d{4})-(\d{2})-(\d{2}).(\d{2}):(\d{2}):(\d{2})\.(\d+).+/i, '$1-$2-$3 $4:$5:$6.$7');
     const levelColor = E_LEVEL_COLORS[level] || E_COLORS.RESET;
     const levelStr = level?.toUpperCase();
-    const dataStr = data ? `(${JSON.stringify(data)})` : '';
+    const dataStr = data ? ` | ${JSON.stringify(data)}` : '';
     let errStr = '';
     if(err){
         errStr += '\n' + (err.stack || err);
